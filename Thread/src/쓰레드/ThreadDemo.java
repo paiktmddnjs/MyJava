ad9708a0dd37934e3dddce5b8d8dc40eb97e345b
paiktmddnjs <paiktmddnjs@naver.com>
2025-09-23 18:02:26 +0900

    no message

--- Thread/src/쓰레드/ThreadDemo.java
+++ Thread/src/쓰레드/ThreadDemo.java
@@ -1,19 +1,20 @@
 package 쓰레드;
 
-class ThreadDemo extends Thread {
-	public void run() {
-		String name = Thread.currentThread().getName();
-		System.out.println(name + " 실행 중!");
-	}
+class MyThread extends Thread {
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " 실행 중!");
+    }
 }
 
 public class ThreadDemo {
-	public static void main(String[] args) {
-		ThreadDemo t1 = new ThreadDemo();
-		ThreadDemo t2 = new ThreadDemo();
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
 
-		t1.start();
-		t2.start();
-		System.out.println("메인 스레드 실행");
-	}
+        t1.start();
+        t2.start();
+
+        System.out.println("메인 스레드 실행");
+    }
 }
